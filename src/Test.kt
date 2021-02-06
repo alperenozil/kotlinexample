d139f429d9fba1f774d6ea55cb2e9810f53991e4
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-02-06 12:24:01 +0300

    update

--- src/Test.kt
+++ src/Test.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>) {
     // val is immutable. same as final in java
-    val num: Int
+    var num: Int
     num=10
+    num=20
+    val employee1=Employee("Alperen",500)
+    employee1.name="ali"
+}
+
+class Employee(var name: String, val id: Int){
+
 }
