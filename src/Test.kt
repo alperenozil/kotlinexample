b6656a44e2d96cf84389643c174065e2134aeab9
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-02-06 16:21:53 +0300

    update

--- src/Test.kt
+++ src/Test.kt
@@ -5,6 +5,8 @@ fun main(args: Array<String>) {
     num=20
     val employee1=Employee("Alperen",500)
     employee1.name="ali"
+    val names = arrayListOf("ali","veli","49","50")
+    for(name in names) print(name+" ")
 }
 
 class Employee(var name: String, val id: Int){
