be2909aaa884db41f0da80d796f5529c47370a06
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-02-07 17:33:13 +0300

    update

--- src/Test.kt
+++ src/Test.kt
@@ -4,15 +4,21 @@ fun main(args: Array<String>) {
     num=10
     num=20
     val employee1=Employee("Alperen",500)
-    employee1.name="ali"
+    val employee2=Employee("Ozil",500)
+    val employee3=Employee("Alperen",500)
     val names = arrayListOf("ali","veli","49","50")
-    for(name in names) print(name+" ")
+    //for(name in names) print(name+" ")
+    println(employee1.equals(employee2))
+    println(employee1==employee3)
 }
 
 class Employee(var name: String, val id: Int){
     override fun equals(obj: Any?): Boolean {
         if(obj is Employee){
+            println("name: "+name+" obj.name: "+obj.name)
+            println("id: "+id+" obj.id: "+obj.id)
             return name==obj.name && id==obj.id
-        } else return false
+        }
+        return false
     }
 }
